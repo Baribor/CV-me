9fde59479190dc6f4bab70ff12dc2e37aca9567a
amusukwa <amusukwa@gmail.com>
2024-02-01 13:01:00 -0800

    model classes

---
+++ server/model/education.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Education(db.Model):
+    """Defines columns for education class"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    institution = db.Column(db.String(100), nullable=False)
+    degree = db.Column(db.String(150), nullable=True)
+    startDate = db.Column(db.Date, nullable=False)
+    endDate = db.Column(db.Date, nullable=False)
+    cv_id = db.Column(db.Integer, db.ForeignKey('cv.id'), nullable=False)
+    cv = db.relationship('CV', back_populates='education')
