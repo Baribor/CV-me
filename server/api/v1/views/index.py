fc539b3a74b98a4704386a37dc0c60394976565e
amusukwa <amusukwa@gmail.com>
2024-02-07 21:52:35 +0000

    routes

--- server/api/v1/views/index.py
+++ server/api/v1/views/index.py
@@ -3,12 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify,Blueprint
+from flask import make_response
+from service.user_service import UserService
+
 
 user_bp = Blueprint('users', __name__)
+cv_bp = Blueprint('cv', __name__)
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def user_status():
     """ Status of users"""
-    return jsonify({"status": "OK"})
+    response_data = {"status": "OK"}
+    response = make_response(jsonify(response_data), 200)
+    return response
 
+@app_views.route('/users/profile', methods=['GET'])
+def view_profile():
+    """ Get user data"""
+    user = get_user_somehow()
+    profile_data = UserService.view_profile(user)
+    return jsonify(profile_data), 200
