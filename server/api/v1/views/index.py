6e2ac315240b538caaccf477e2ddf1f277f4fb1b
amusukwa <amusukwa@gmail.com>
2024-02-07 14:03:01 -0800

    updates

--- server/api/v1/views/index.py
+++ server/api/v1/views/index.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+from flask import Blueprint, jsonify
+
+# Create a Blueprint instance
+app_views = Blueprint('app_views', __name__)
+
+
+@app_views.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to CV-me API'})
+
+from .user_views import user_bp
+from .cv_views import cv_bp
+
+# Register blueprints for other views
+app_views.register_blueprint(user_bp)
+app_views.register_blueprint(cv_bp)
 
--- tests/test_models/test_sample.py
+++
@@ -1,5 +0,0 @@
-# tests/test_simple.py
-from flask_sqlalchemy import SQLAlchemy
-
-def test_import():
-    assert SQLAlchemy is not None
