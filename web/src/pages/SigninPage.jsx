def8b67f97ce03df2ac3d43094192bcf9991f42d
Baribor Saturday <saturdaybaribor@gmail.com>
2024-02-08 22:10:10 +0100

    Added seo component

--- web/src/pages/SigninPage.jsx
+++ web/src/pages/SigninPage.jsx
@@ -1,26 +1,30 @@
+import SEO from "../components/SEO";
 
 
 const SignInPage = () => {
 
 	return (
-		<div className="flex">
-			<div className="flex items-center bg-primary_light">
-				< img src="login.svg" alt="" className="h-2/3" />
-			</div >
-			<div className="flex-grow">
-				<div className="flex flex-col mt-20 h-full px-10">
-					<h1 className="font-bold text-primary text-3xl self-center">LOGIN</h1>
-					<label htmlFor="email">Email</label>
-					<input type="email" name="email" id="" className="border-primary border h-10 rounded-lg focus:border-2 px-2 select-none outline-none" />
-					<label htmlFor="password">Password</label>
-					<input type="password" name="password" id="" className="border-primary border h-10 rounded-lg focus:border-2 px-2 select-none outline-none" />
-					<span className="text-primary cursor-pointer text-sm self-end my-2">Forgot password?</span>
-					<button className="bg-primary text-white py-2 rounded-lg">LOGIN</button>
-					<p className="my-2">Don&apos;t have an acount? <span className="text-primary cursor-pointer text-sm self-end ">Sign up</span></p>
+		<>
+			<SEO title={"login"} />
+			<div className="flex">
+				<div className="flex items-center bg-primary_light">
+					< img src="login.svg" alt="" className="h-2/3" />
+				</div >
+				<div className="flex-grow">
+					<div className="flex flex-col mt-20 h-full px-10">
+						<h1 className="font-bold text-primary text-3xl self-center">LOGIN</h1>
+						<label htmlFor="email">Email</label>
+						<input type="email" name="email" id="" className="border-primary border h-10 rounded-lg focus:border-2 px-2 select-none outline-none" />
+						<label htmlFor="password">Password</label>
+						<input type="password" name="password" id="" className="border-primary border h-10 rounded-lg focus:border-2 px-2 select-none outline-none" />
+						<span className="text-primary cursor-pointer text-sm self-end my-2">Forgot password?</span>
+						<button className="bg-primary text-white py-2 rounded-lg">LOGIN</button>
+						<p className="my-2">Don&apos;t have an acount? <span className="text-primary cursor-pointer text-sm self-end ">Sign up</span></p>
 
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
