b25a5b0bcf879b0edc669a151d6640b613b73268
amusukwa <amusukwa@gmail.com>
2024-02-12 13:46:44 +0000

    cv

--- server/api/v1/views/cv_views.py
+++ server/api/v1/views/cv_views.py
@@ -8,23 +8,36 @@ from service.cv_service import CvService
 
 
 @app_views.route("/cv", methods=['POST'])
-def create_cv():
-    """ Create a new user"""
-    return jsonify({'error': 'Failed to create user'}), 500
+def create_cv(cv_data):
+    """ Create a new cv"""
+     title = cv_data.get('title')
+    user_id = cv_data.get('user_id')
 
-@app_views.route('/cv/<int:user_id>', methods=['GET'])
+    # Check if title and user_id are provided
+    if not title or not user_id:
+        return jsonify({'error': 'Title and user ID are required'}), 400
+
+    # Create a new CV instance
+    new_cv = CV(
+        title=title,
+        user_id=user_id,
+        createdAt=datetime.utcnow(),  
+        UpdateddAt=datetime.utcnow()
+        
+
+@app_views.route('/cv/<int:cv_id>', methods=['GET'])
 def get_cv(cv_id):
     """Get CV"""
-    cv = CvService.get_cv(user_id)
+    cv = CvService.get_cv(cv_id)
     if cv:
         return jsonify(cv), 200
     else:
         return jsonify({"error": "CV not found"}), 404
 
-@app_views.route('/cv/<int:user_id>', methods=['DELETE'])
+@app_views.route('/cv/<int:cv_id>', methods=['DELETE'])
 def delete_cv(cv_id):
     """Delete CV"""
-    if CvService.delete_cv(user_id):
+    if CvService.delete_cv(cv_id):
         return jsonify({"message": "CV deleted successfully"}), 200
     else:
         return jsonify({"error": "Failed to delete CV"}), 400
